d0543889c80cf20e77b645ae256e35ff331087af
audiobird <cantrellhepner@gmail.com>
2024-01-26 21:37:03 -0800

    fix up

--- audiobird/squareosc.hh
+++ audiobird/squareosc.hh
@@ -11,14 +11,14 @@ template <typename T, uint32_t sample_rate_hz,
           uint8_t phase_step_table_size_bits>
 class Square : public Phaser<sample_rate_hz, phase_step_table_size_bits> {
   static_assert(std::is_same_v<T, int16_t> || std::is_same_v<T, float>);
-  using Phaser = Phaser<sample_rate_hz, phase_step_table_size_bits>;
-  static_assert(std::is_same_v<typename Phaser::type, uint16_t>);
+  using mtPhaser = Phaser<sample_rate_hz, phase_step_table_size_bits>;
+  static_assert(std::is_same_v<typename mtPhaser::type, uint16_t>);
 
-  typename Phaser::type pulsewidth;
+  mtPhaser::type pulsewidth;
 
 public:
-  T Update() {
-    const auto p = Phaser::update();
+  T update() {
+    const auto p = mtPhaser::update();
     const auto high = p >= (32768 + pulsewidth);
     if constexpr (std::is_same_v<T, float>) {
       return high ? 1.f : -1.f;
@@ -26,7 +26,10 @@ public:
       return high ? 32767 : -32767;
     }
   }
-  int8_t setPulseWidth(int8_t pw) { pulsewidth = pw << 8; }
+  int8_t setPulseWidth(int8_t pw) {
+    pulsewidth = pw << 8;
+    return pw;
+  }
   float setPulseWidth(float pw) {
     const auto o = std::clamp(pw, -1.f, 1.f);
     pulsewidth = pw * 32767;
